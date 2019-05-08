b03c5f940da14cfffad003c8ce7e64652c4c8a20
Zac Crosby <Zacros7164@gmail.com>
2019-05-08 12:14:25 -0400

    check for same words

--- backEnd/routes/index.js
+++ backEnd/routes/index.js
@@ -10,23 +10,32 @@ router.get('/', function (req, res, next) {
 
 router.post("/anagramTest", (req, res) => {
   const { word1, word2 } = req.body;
-
-  let sorted1 = word1.split("").sort();
-  let sorted2 = word2.split("").sort();
+  let word1Array = word1.split("")
+  let word2Array = word2.split("")
+  let sorted1 = [...word1Array].sort();
+  let sorted2 = [...word2Array].sort();
   let results = true;
+  console.log(word2Array)
+  console.log(sorted2)
 
-  if(sorted1.length == sorted2.length){
-    for(let i = 0; i <= sorted1.length; i++){
-      if(sorted1[i] != sorted2[i]){
-        results = false;
-        break
-      }else{
-        continue
+  if (word1Array[0] != word2Array[0]) {
+    if (sorted1.length == sorted2.length) {
+      for (let i = 0; i <= sorted1.length; i++) {
+        if (sorted1[i] != sorted2[i]) {
+          results = false;
+          break
+        } else {
+          continue
+        }
       }
+    } else {
+      results = false;
     }
   }else{
     results = false;
   }
+
+
 
   res.json({
     results
